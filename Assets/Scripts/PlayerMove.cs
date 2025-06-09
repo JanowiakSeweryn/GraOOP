8b2a537ded84b3532d10169569e78d73710a98f2
Janowiak Seweryn <janowiakseweryn@gmail.com>
2025-06-09 19:25:26 +0200

    Added movement with sigma Camil

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,27 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMove: MonoBehaviour
 {
-     public float speed = 5f;
-   
-      public Raycast ray_cast;
+
+    public Transform groundCheck;
+    public float groudDistance = 0.4f;
+    public LayerMask grounMask;
+
+    public float jumpHeight = 3f;
+
+    bool isGrounded;
 
 
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -19.62f;
+
+    Vector3 velocity;
+
+    // Update is called once per frame
     void Update()
     {
-        if(!ray_cast.locked){
-            float horizontalInput = 0f;
-            float verticalInput = 0f;
 
-            if (Input.GetKey(KeyCode.W)) verticalInput = 1f;
-            if (Input.GetKey(KeyCode.S)) verticalInput = -1f;
-            if (Input.GetKey(KeyCode.D)) horizontalInput = 1f;
-            if (Input.GetKey(KeyCode.A)) horizontalInput = -1f;
+        isGrounded = Physics.CheckSphere(groundCheck.position, groudDistance, grounMask);
 
-            Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput).normalized * speed * Time.deltaTime;
-
-            transform.Translate(movement, Space.Self);
+        if(isGrounded == true && velocity.y < 0)
+        {
+            velocity.y = -2f;
         }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
