f4f5d021b8aaa53b471badbd1cb3e669d45453cd
JanowiakSeweryn <janowiakseweryn@gmail.com>
2025-06-17 15:31:11 +0200

    Tera Dziala Wszystko
    strzelanie
    interface
    pingwyny nie dzialajce do fulla

--- Assets/Scripts/Shotting/Target.cs
+++ Assets/Scripts/Shotting/Target.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Target : MonoBehaviour, IDamagable
+public class Target : MonoBehaviour,  IDamagable
 {
 
     private float health = 100f;
